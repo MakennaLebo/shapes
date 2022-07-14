99cf5f5a46e7bd1fd028e3a9f1104d9cbb710cf0
MakennaLebo <kennalebo@hotmail.com>
2022-07-14 13:55:33 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function getAreaOfCircle (radius){
+    return (radius * radius * Math.PI);
+}
+
+console.log(getAreaOfCircle(7));
+
+function getCircumferenceOfCircle (radius){
+    return (2 * Math.PI * radius);
+}
+
+console.log(getCircumferenceOfCircle(7));
+
+function getAreaOfSquare(side){
+    return(side * side);
+}
+
+console.log(getAreaOfSquare(7));
+
+function getAreaOfTriangle(base, height){
+    return (base * height / 2);
+    
+}
+
+console.log(getAreaOfTriangle(7,8));
